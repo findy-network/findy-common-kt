fe45e29e648ae031410c9e39960a71935d53dc76
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-01-24 15:01:09 +0200

    Add versioning.

---
+++ tools/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+git config --global user.name release-bot
+git config --global user.email release-bot
+
+VERSION_NBR=$(cat VERSION)
+if [ -z "$VERSION_NBR" ]; then
+    VERSION_NBR="0.0.1"
+fi
+
+NEW_VERSION=$1
+if [ -z "$NEW_VERSION" ]; then
+    NEW_VERSION="${VERSION_NBR%.*}.$((${VERSION_NBR##*.} + 1))"
+fi
+
+echo "Attempt to release version $NEW_VERSION"
+
+echo $NEW_VERSION >VERSION
+git add VERSION
+git commit -a -m "Increase version to v$NEW_VERSION."
+git push origin master
